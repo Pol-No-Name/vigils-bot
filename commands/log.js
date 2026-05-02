9063f94e4574510a08bd3be0a5521c9252bc8b61
Pol-No-Name <chriscristi2005@gmail.com>
2026-05-02 16:48:25 +0100

    logging

---
+++ commands/log.js
@@ -0,0 +1,51 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const sheetdbapi = process.env.SHEETDB_API;
+const axios = require('axios');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('log')
+        .setDescription('Logs a report to the spreadsheet')
+        .addUserOption(option =>
+            option.setName('user')
+                .setDescription('The user to report')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('reason')
+                .setDescription('The reason for the report')
+                .setRequired(true)),
+
+     async execute(interaction) {
+        const user = interaction.options.getUser('user');
+        const username = user.username;
+        const userId = user.id;
+        const reason = interaction.options.getString('reason');
+        const timestamp = new Date().toLocaleString('en-UK', { timeZone: 'UTC', hour12: false });
+
+        try {
+            await axios.post(sheetdbapi, {
+                data: {
+                    USER: username,
+                    USERID: userId,
+                    REASON: reason,
+                    TIMESTAMP: timestamp + ' UTC'
+                }
+            });
+            embed = new EmbedBuilder()
+                .setTitle('Report Logged')
+                .setColor(0xeb4034)
+                .setDescription(`Report for <@${userId}> has been logged successfully.`)
+                .addFields(
+                    { name: 'User', value: `<@${userId}>`, inline: true },
+                    { name: 'Reason', value: reason, inline: true }
+            )
+            .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+
+        } catch (error) {
+            console.error('Error logging to SheetDB:', error);
+            return interaction.reply({ content: 'Failed to log the report. Please try again later.', ephemeral: true });
+        }
+     }
+}
