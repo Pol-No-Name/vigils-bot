96407ebdce2aaa4071072930417b6d3313fe32a3
Pol-No-Name <chriscristi2005@gmail.com>
2026-06-04 14:56:06 +0100

    Updated HELP command

--- commands/help.js
+++ commands/help.js
@@ -18,14 +18,18 @@ module.exports = {
             .addFields(
                 { name: '/check <user>', value: 'Check your or someone else\'s honor points.' },
                 { name: '/leaderboard', value: 'Shows top users by honor points.' },
-                { name: '/ping', value: 'Replies with pong.' },
+                { name: '/lore', value: 'Check the lore of another guild member or yourself, lore is earned by making a name for yourself.' },
+                { name: '/ranks', value: 'Show the requirement to rank up.' },
+                { name: '/shop', value: 'Show the items available in the guild shop.' },
                 { value: '**---- Admin Commands ----**', name: '\u200B' },
                 { name: '/reset', value: 'Reset all honor points to 0' },
-                { name: '/subtract <user> <amount>', value: 'Remove honor points from a user.' },
+                { name: '/remove <user> <amount>', value: 'Remove honor points from a user.' },
                 { name: '/give <user> <amount>', value: 'Give honor points to a user.' },
+                { name: '/loreAdd <user> <lore>', value: 'Add lore to a user.' },
+                { name: '/log <user> <reason>', value: 'Log a user' },
                 { value: '**---- Honor Info ----**', name: '\u200B' },
-                { name: 'How do I earn honor?', value: 'Honor is earned by doing tasks for the guild. Check PLACEHOLDER for more information.'},
-                { name: 'What can I do with honor?', value: 'Honor can be used to buy items from the guild shop. Check PLACEHOLDER for more information.'}
+                { name: 'How do I earn honor?', value: 'Honor is earned by doing tasks for the guild. Check <#1503122959294926919> for more information.'},
+                { name: 'What can I do with honor?', value: 'Honor can be used to buy items from the guild shop. Use /shop for more information.'}
             )
             .setColor(0x89b9e0);
         await interaction.reply({ embeds: [embed] }); 
