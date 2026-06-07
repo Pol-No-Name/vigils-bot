1ea94c2932de36783afab2ba41094d876d06b20d
Pol-No-Name <chriscristi2005@gmail.com>
2026-06-07 19:12:45 +0100

    fix id?

--- commands/rankup.js
+++ commands/rankup.js
@@ -4,7 +4,7 @@ const rankRequirements = [
     { rank: 'Initiate', req: "none", roleId: '1361370411241050243' },
     { rank: 'Swordsman', req: "**25 Honor**, Have the uniform.", roleId: '1361370385244749926' },
     { rank: 'Advanced Swordsman', req: "**50 Honor**, Attend 2 WFTE events. Have a vouch from Instructor+", roleId: '1438837496082599946' },
-    { rank: 'Captain', req: "**100 Honor**, Attend 2 WFTE events. Have a vouch from a Sentinel+ as well as passing a special tryout.", roleId: '1505316830678356049' },
+    { rank: 'Captain', req: "**100 Honor**, Attend 2 WFTE events. Have a vouch from a Sentinel+ as well as passing a special tryout.", roleId: '1505531067388657745' },
     { rank: 'Instructor', req: "**150 Honor**, Attend 3 WFTE events. Have tryoutted multiple people, approved by either Stratos or Maestro.", roleId: '1361370380773621770' },
     { rank: 'Blademaster', req: "**250 Honor**, Attend 3 WFTE events. Must have hosted at least 2 events, MVP in one WFTE event and approval by either Maestro or Stratos.", roleId: '1438836488346275870' },
     { rank: 'Sentinel', req: "A Sentinel is specifically chosen by Maestro.", roleId: '1438836688305651792' }
