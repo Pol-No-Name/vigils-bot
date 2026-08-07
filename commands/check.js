cb3afd5c0b7eedadcfe51dc02287fda70378d8e0
Pol-No-Name <chriscristi2005@gmail.com>
2026-08-07 18:15:13 +0100

    upd1.25

--- commands/check.js
+++ commands/check.js
@@ -1,23 +1,25 @@
 const { SlashCommandBuilder } = require('discord.js');
 const points = require('../hnManager');
-const member = interaction.options.getMember('user') || interaction.member;
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('profile')
         .setDescription("Check your own profile or another person's profile")
         .addUserOption(option =>
-            option.setName('user')
+            option
+                .setName('user')
                 .setDescription('User to check the profile of')
                 .setRequired(false)
         ),
 
     async execute(interaction) {
         const user = interaction.options.getUser('user') || interaction.user;
+        const member = interaction.options.getMember('user') || interaction.member;
+
         const pointsValue = points.getPoints(user.id);
 
         const embed = {
-            title: `${member.nickname || user.username}'s Profile`,
+            title: `${member.displayName}'s Profile`,
             description: `**${pointsValue}** honor points\n`,
             color: 0x89b9e0
         };
