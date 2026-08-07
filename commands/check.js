38f09c69ff08d17779a8e143b8922ff227fc1998
Pol-No-Name <chriscristi2005@gmail.com>
2026-08-07 18:07:47 +0100

    upd1.1

--- commands/check.js
+++ commands/check.js
@@ -16,7 +16,7 @@ module.exports = {
         const pointsValue = points.getPoints(user.id);
 
         const embed = {
-            title: `${user.displayName}'s Profile`,
+            title: `${user.nickname || user.username}'s Profile`,
             description: `**${pointsValue}** honor points\n`,
             color: 0x89b9e0
         };
